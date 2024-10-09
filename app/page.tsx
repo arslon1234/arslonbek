bc4afb0e42ff022fd5250621e2d3db35d5387c3e
Arslonbek <roziboyevarslon0110@gmail.com>
2024-10-09 20:28:21 +0500

    replace Head with metaData

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,31 @@
-// import Image from "next/image";
-import Head from "next/head";
 import HomePage from "@/components/home";
+import type { Metadata } from "next";
+
+// Define the metadata
+export const metadata: Metadata = {
+  title: "Arslonbek Ro'ziboyev - Personal Portfolio",
+  description:
+    "Welcome to Arslonbek Ro'ziboyev's personal portfolio. Learn more about my projects, experience, and skills.",
+  keywords:
+    "Arslonbek Ro'ziboyev, Personal Portfolio, Web Developer, Next.js, React.js, Vue.js, Nuxt.js, SEO, Software Engineer",
+  openGraph: {
+    title: "Arslonbek Ro'ziboyev - Personal Portfolio",
+    description:
+      "Explore my portfolio, featuring my projects, blog, and contact information.",
+    url: "https://arslonbek.com",
+    images: [
+      {
+        url: "/static/awesome-image.png",
+        alt: "Arslonbek Ro'ziboyev's Portfolio Image",
+      },
+    ],
+  },
+};
+
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Arslonbek Ro`ziboyev - Personal Portfolio</title>
-        <meta
-          name="description"
-          content="Welcome to Arslonbek Ro'ziboyev's personal portfolio. Learn more about my projects, experience, and skills."
-        />
-        <meta
-          name="keywords"
-          content="Arslonbek Ro'ziboyev, Personal Portfolio, Web Developer, Next.js, React.js, Vue.js, Nuxt.js, SEO, Software Engineer"
-        />
-        <meta
-          property="og:title"
-          content="Arslonbek Ro'ziboyev - Personal Portfolio"
-        />
-        <meta
-          property="og:description"
-          content="Explore my portfolio, featuring my projects, blog, and contact information."
-        />
-        <meta property="og:image" content="/static/awesome-image.png" />
-        <meta property="og:url" content="https://arslonbek.com" />
-      </Head>
-      <>
-        <HomePage/>
-      </>
+      <HomePage />
     </>
   );
 }
