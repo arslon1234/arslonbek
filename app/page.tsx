cbf5a0270404876144f79d7e3a82171e8d4c3431
Arslonbek <roziboyevarslon0110@gmail.com>
2024-10-24 11:52:21 +0500

    favicon deleted

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import type { Metadata } from "next";
 
 // Define the metadata
 export const metadata: Metadata = {
-  title: "Arslonbek Ro'ziboyev - Personal Portfolio",
+  title: "Arslonbek's Blog",
   description:
     "Welcome to Arslonbek Ro'ziboyev's personal portfolio. Learn more about my projects, experience, and skills.",
   keywords:
     "Arslonbek Ro'ziboyev, arslonbek, arslon, Personal Portfolio, Web Developer, Next.js, React.js, Vue.js, Nuxt.js, SEO, Software Engineer",
   openGraph: {
-    title: "Arslonbek Ro'ziboyev - Personal Portfolio",
+    title: "Arslonbek's Blog",
     description:
       "Explore my portfolio, featuring my projects, blog, and contact information.",
     url: "https://arslonbek.com",
